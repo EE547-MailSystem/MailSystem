7783aba35a7d95837e07dbcca4c89a64258c586c
Yicheng Yang <yangyich@usc.edu>
2025-04-18 14:16:20 -0700

    email controller demo

--- backend/src/dao/emailDao.js
+++ backend/src/dao/emailDao.js
@@ -1,47 +1,48 @@
-const { DynamoDBClient, ListTablesCommand } = require('@aws-sdk/client-dynamodb');
-const { DynamoDBDocumentClient, TransactWriteCommand } = require('@aws-sdk/lib-dynamodb');
+const {
+  DynamoDBClient,
+  ListTablesCommand,
+} = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  TransactWriteCommand,
+} = require("@aws-sdk/lib-dynamodb");
 
-class Dynamo_Dao{
-  constructor(tableName = 'email') {
-      this.tableName = tableName;
-      try{
-          const client = new DynamoDBClient({ 
-              region: 'us-east-2',
-          });
-          this.docClient = DynamoDBDocumentClient.from(client);
-      }
-      catch(error){
-          console.error('connecting error:', error);
-      }
+class Email_Dao {
+  constructor(tableName = "email") {
+    this.tableName = tableName;
+    try {
+      const client = new DynamoDBClient({
+        region: "us-east-2",
+      });
+      this.docClient = DynamoDBDocumentClient.from(client);
+    } catch (error) {
+      console.error("connecting error:", error);
+    }
   }
 
   async storeEmail(emailData) {
-      // wrap the command with condition to avoid data duplication
-      const params = {
-        TransactItems: [
-          {
-            Put: {
-              TableName: this.tableName,
-              Item: emailData,
-              ConditionExpression: 'attribute_not_exists(email_id)' // set condition to avoid data duplication
-            }
-          }
-        ]
-      };
+    // wrap the command with condition to avoid data duplication
+    const params = {
+      TransactItems: [
+        {
+          Put: {
+            TableName: this.tableName,
+            Item: emailData,
+            ConditionExpression: "attribute_not_exists(email_id)", // set condition to avoid data duplication
+          },
+        },
+      ],
+    };
 
-      const command = new TransactWriteCommand(params);
-      try {
-          await this.docClient.send(command);
-      } catch (error) {
-          console.error('Error storing email:', error);
-          throw error;
-      }
+    const command = new TransactWriteCommand(params);
+    try {
+      await this.docClient.send(command);
+    } catch (error) {
+      console.error("Error storing email:", error);
+      throw error;
+    }
   }
-
-
-  
-
 }
 
 // export this
-module.exports = new Dynamo_Dao(); 
+module.exports = new Email_Dao();
